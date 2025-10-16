7c9c5b02a36a901d352857ec361479304bcca9fb
soumya625 <51965945+soumya625@users.noreply.github.com>
2025-10-16 13:35:41 -0400

    Update setup-nginx.sh

--- static-website-nginx-aws/scripts/setup-nginx.sh
+++ static-website-nginx-aws/scripts/setup-nginx.sh
@@ -15,7 +15,7 @@ sudo rm -rf /var/www/html/*
 sudo cp -r /home/ubuntu/static-website-nginx-aws/* /var/www/html/
 
 #set permission so nginx can read 
-sudo chown -R www-data:www:data /var/www/html
+sudo chown -R www-data:www-data /var/www/html
 sudo chmod -R 755 /var/www/html
 
 # Restart Nginx
