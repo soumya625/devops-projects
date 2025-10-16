6a76861e01a560b46b4770d5bb6902056a865f89
soumya625 <51965945+soumya625@users.noreply.github.com>
2025-10-16 11:49:50 -0400

    Update setup-nginx.sh

--- static-website-nginx-aws/scripts/setup-nginx.sh
+++ static-website-nginx-aws/scripts/setup-nginx.sh
@@ -11,15 +11,9 @@ sudo systemctl enable nginx
 # Remove default Nginx files
 sudo rm -rf /var/www/html/*
 
-# Clone website files from GitHub (to /tmp first)
-cd /tmp
-sudo git clone https://github.com/<your-username>/static-website-nginx-aws.git
-
-# Copy website files to Nginx web root
-sudo cp -r static-website-nginx-aws/* /var/www/html/
-
-# Fix permissions
-sudo chmod -R 755 /var/www/html
+# Clone website files from GitHub 
+cd /var/www/html
+sudo git clone https://github.com/soumya625/static-website-nginx-aws.git .
 
 # Restart Nginx
 sudo systemctl restart nginx
